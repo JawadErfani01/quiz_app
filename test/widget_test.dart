79276e4c00d7b697937038a4e6c4a4cb581dbba7
Jawad Erfani <jawad.erfani147@gmail.com>
2022-11-06 01:08:29 +0430

    main. dart changed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:testproject/main.dart';
+import 'package:quizapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
